8650d61fceef102d7438313c7d1ff6f044ca989d
m1tk <mohammed.salih2@usmba.ac.ma>
2024-08-24 14:22:22 +0000

    Adding initial server layout

---
+++ server/src/main.py
@@ -0,0 +1,12 @@
+import os
+from fastapi import FastAPI
+
+if "PRODUCTION" in os.environ:
+    app = FastAPI(docs_url=None, redoc_url=None)
+else:
+    app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return { "message": "Hello World" }
