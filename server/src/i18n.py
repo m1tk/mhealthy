6acc1f9476e5c9d42ab357a68f8b79975c64731d
m1tk <mohammed.salih2@usmba.ac.ma>
2024-09-06 02:38:27 +0000

    proper setup for internationalization

--- server/src/i18n.py
+++ server/src/i18n.py
@@ -5,5 +5,9 @@ class i18nMiddleware(BaseHTTPMiddleware):
     # for now default locale is fr
     
     async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
-        request.state.locale = "fr"
+        locale = request.headers.get('Accept-Language', None) or 'en'
+
+        if locale not in ["en", "fr"]:
+            locale = "en"
+        request.state.locale = locale
         return await call_next(request)
