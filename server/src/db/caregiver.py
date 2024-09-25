f88192022373a9efcff57ef9389831993039de31
Mohammed Salih <92683109+m1tk@users.noreply.github.com>
2024-09-25 14:16:07 +0100

    Tell caregiver to stop if they are unassigned

--- server/src/db/caregiver.py
+++ server/src/db/caregiver.py
@@ -70,6 +70,9 @@ where patient = $1 and id > $2 order by id asc;
                             del inst["new_patient"]
                     elif inst["type"] == "unassign_caregiver" and caregiver == inst["id"]:
                         del inst["id"]
+                        isassigned = await con.fetchval("select exists(select 1 from assigned where caregiver = $1 and patient = $2);", caregiver, patient)
+                        if not isassigned:
+                            inst["stop"] = true
 
                 yield Instruction(
                     instruction=inst,
@@ -78,7 +81,7 @@ where patient = $1 and id > $2 order by id asc;
                 )
 
                 if "type" in inst and inst["type"] == "unassign_caregiver" and "id" not in inst:
-                    if not await con.fetchval("select exists(select 1 from assigned where caregiver = $1 and patient = $2);", caregiver, patient):
+                    if not :
                         yield None
                         break
 
